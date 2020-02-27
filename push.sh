20b8f76df02aeb9a5a5f6d5d16010b4cd41fd9e8
paulmazeranensg <61426454+paulmazeranensg@users.noreply.github.com>
2020-02-27 18:58:49 +0100

    Create push.sh

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "mazeran.paul@gmail.com"
+  git config --global user.name "paulmazeranensg"
+}
+
+commit_website_files() {
+  git checkout -b gh-pages
+  git add . *.html
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${59825589046d4fb5a994c0e862024006a8bb2cee}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages 
+}
+
+setup_git
+commit_website_files
+upload_files
